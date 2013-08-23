28cde20cd99ade74fd57486ca048ed067f204a6b
hejhansson <hejhansson@me.com>
2013-08-23 15:52:48 +0200

    fixes

--- incl/functions.js
+++ incl/functions.js
@@ -1,7 +1,31 @@
+
+/* --------------------------------------------------------------
+	$THE MENU
+-------------------------------------------------------------- */
+
 window.onload = function() {
+		
+		var menuRef = document.getElementById("menu");
+		menuRef.onclick = function() {
+		var navRef = document.getElementById("theNav");
+		var theClassRef = navRef.getAttribute("class");
+		toggleMenu(theClassRef);
+		}
 
-	/*
-		YOUR CODE
-	*/
+		function toggleMenu(classIn) {
+			if (classIn == "closed") {
+				var navRef = document.getElementById("theNav");
+				navRef.setAttribute("class", "open");
+			}
 
+			else {
+				var navRef = document.getElementById("theNav");
+				navRef.setAttribute("class", "closed");
+			}
+		}
 }
+
+/* --------------------------------------------------------------
+	$YOUR FUNCTIONS
+-------------------------------------------------------------- */
+
