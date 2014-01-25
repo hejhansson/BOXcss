22e16d8b7cc3006e899ac155fdf8f9f450c8c5dc
hejhansson <hejhansson@me.com>
2014-01-25 14:09:45 +0100

    New banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
       less: 'less/_box.less',
       css: 'css/box.css',
       cssMin: 'css/box.min.css',
-      banner: '/* \n BOXcss <%= pkg.version %> by Alexander Hansson \n URL: https://github.com/hejhansson/BOXcss \n Theme Name: \n Theme URI: http:// \n Author: \n Author URI: http://alexanderhansson.se/ \n Description: \n Version: 1.0 \n License: \n Tags: \n */\n'
+      banner: '/*! BOXcss <%= pkg.version %> |  https://github.com/hejhansson/BOXcss */\n'    
     },
 
     // Tasks
