0c97410fb042f535b0eae72cac62c153bcf00579
hejhansson <hejhansson@me.com>
2014-08-19 21:41:17 +0200

    Favicons

--- js/functions.js
+++ js/functions.js
@@ -3,9 +3,24 @@ $(document).ready(function() {
 	
 	console.log("i love lamp");
 
-	var win   = $(window);
-	var doc   = $(document);
-	var body  = $('body');
-	var $self = $(this);
+});
 
+
+
+/*
+ * Confirm with users before opening
+ * any mailto:-link through their default email client.
+ */
+
+$("a").click(function (e) {
+    var link = $(this).attr("href").substring(0, 7);
+
+    if(link == "mailto:") {
+        var result = window.confirm("Do you want to open your email client?");
+
+        if(result == false) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    }
 });
